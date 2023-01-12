fb61edc5a3244cdf88b77d58880ac57b3bdcb00b
Andrew Clinton <ddrew.clinton@gmail.com>
2023-01-12 11:13:26 -0500

    moar tests

--- tests/Unit/FormConfigTest.php
+++ tests/Unit/FormConfigTest.php
@@ -63,11 +63,12 @@ class FormConfigTest extends TestCase
         $this->assertCount(0, $form_config->get('test_form'));
     }
 
-    public function test_catch_missing_url_exception()
+    public function test_catch_missing_url_exception_for_crm()
     {
         Config::set('statamic-forms.forms.test_form', [
             'crm::sales-force' => [
                 '::fake'      => false,
+                '::enabled'   => true,
                 '::fake-type' => 'success',
                 'default'     => [
                     'lead_source' => fake()->company,
@@ -80,9 +81,70 @@ class FormConfigTest extends TestCase
 
         $form_config = new FormConfig();
 
-        //todo[aclinton] - this is not working
-        $this->assertThrows(function () use($form_config) {
+        $this->assertThrows(function () use ($form_config) {
             $form_config->get('test_form');
         }, MissingManagerConfigParamException::class);
+    }
+
+    public function test_local_url_overrides_global_url_for_crm_manager()
+    {
+        Config::set('statamic-forms.forms.test_form', [
+            'crm::sales-force' => [
+                '::fake'      => false,
+                '::enabled'   => true,
+                '::fake-type' => 'success',
+                '::url'       => fake()->url,
+                'default'     => [
+                    'lead_source' => fake()->company,
+                ],
+                'maps'        => [
+                    'crm_1' => 'email'
+                ]
+            ]
+        ]);
+
+        Config::set('statamic-forms.defaults', [
+            'crm::sales-force' => [
+                '::url' => fake()->url
+            ]
+        ]);
+
+        $form_config = new FormConfig();
+        $managers    = $form_config->get('test_form');
+        $crm_manager = $managers->first();
+
+        $this->assertEquals(
+            $crm_manager->url,
+            Config::get('statamic-forms.forms.test_form.crm::sales-force.::url')
+        );
+    }
+
+    public function test_global_url_with_no_local_crm_url()
+    {
+        Config::set('statamic-forms.forms.test_form', [
+            'crm::sales-force' => [
+                '::fake'      => false,
+                '::enabled'   => true,
+                '::fake-type' => 'success',
+                'default'     => [
+                    'lead_source' => fake()->company,
+                ]
+            ]
+        ]);
+
+        Config::set('statamic-forms.defaults', [
+            'crm::sales-force' => [
+                '::url' => fake()->url
+            ]
+        ]);
+
+        $form_config = new FormConfig();
+        $managers    = $form_config->get('test_form');
+        $crm_manager = $managers->first();
+
+        $this->assertEquals(
+            $crm_manager->url,
+            Config::get('statamic-forms.defaults.crm::sales-force.::url')
+        );
     }
 }
