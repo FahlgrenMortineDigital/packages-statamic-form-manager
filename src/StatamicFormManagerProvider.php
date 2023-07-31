f7dbbe7d8066d42cef7e7b0a26705c9db5741100
Andrew Clinton <ddrew.clinton@gmail.com>
2023-07-31 13:41:05 -0400

    add grouping

--- src/StatamicFormManagerProvider.php
+++ src/StatamicFormManagerProvider.php
@@ -11,7 +11,7 @@ class StatamicFormManagerProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/../config/statamic-form-manager.php' => config_path('statamic-form-manager.php'),
             __DIR__ . '/../config/statamic-forms.php'        => config_path('statamic-forms.php'),
-        ]);
+        ], 'statamic-form-manager-config');
     }
 
     public function register()
