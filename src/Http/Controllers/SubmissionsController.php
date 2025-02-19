96a7db7692c74372449c0efea530dcc0ec30bb4d
Andrew Clinton <ddrew.clinton@gmail.com>
2025-02-19 16:10:13 -0500

    fix submission controller type hinting

--- src/Http/Controllers/SubmissionsController.php
+++ src/Http/Controllers/SubmissionsController.php
@@ -2,6 +2,7 @@
 
 namespace Fahlgrendigital\StatamicFormManager\Http\Controllers;
 
+use Fahlgrendigital\StatamicFormManager\Contracts\SubmissionInterface;
 use Fahlgrendigital\StatamicFormManager\Data\Export;
 use Statamic\Facades\FormSubmission;
 
@@ -10,7 +11,10 @@ class SubmissionsController
     public function show($submission): \Illuminate\View\View
     {
         $submission = FormSubmission::find($submission);
-        $exports    = Export::forSubmission($submission)->get();
+
+        abort_if(!$submission, 404);
+
+        $exports    = Export::forSubmission(app(SubmissionInterface::class, ['submission' => $submission]))->get();
         $completed  = $exports->filter(fn(Export $export) => $export->completed())->count() === $exports->count();
 
         return view('formidable::submission', compact('submission', 'exports', 'completed'));
