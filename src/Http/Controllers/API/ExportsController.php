2393202253916c321be71af1a9f2c9d8ad997293
Andrew Clinton <aclinton@theshipyard.com>
2026-02-13 22:31:55 -0500

    get exports API working

--- src/Http/Controllers/API/ExportsController.php
+++ src/Http/Controllers/API/ExportsController.php
@@ -3,23 +3,29 @@
 namespace Fahlgrendigital\StatamicFormManager\Http\Controllers\API;
 
 use Fahlgrendigital\StatamicFormManager\Data\Export;
-use Fahlgrendigital\StatamicFormManager\Http\Resources\Api\ExportResource;
+use Fahlgrendigital\StatamicFormManager\Http\Resources\ExportCollection;
 use Statamic\Query\Scopes\Filters\Concerns\QueriesFilters;
+use Statamic\Http\Requests\FilteredRequest;
 
 class ExportsController
 {
     use QueriesFilters;
 
-    public function index()
+    public function index(FilteredRequest $request)
     {
-        $query = Export::query()->when(request('search'), function ($query, $search) {
-            $query->where('form_handle', 'like', "%{$search}%")->orWhere('submission_payload', 'like', "%{$search}%");
-        })->forIndexPage();
+        $query = Export::query()->when($request->search, function ($query, $search) {
+                    $query->where('form_handle', 'like', "%{$search}%")
+                        ->orWhere('submission_payload', 'like', "%{$search}%");
+                })->forIndexPage();
+        $activeFilterBadges = $this->queryFilters(
+            $query, 
+            $request->filters
+        );
 
         $sortField     = request('sort');
         $sortDirection = request('order', 'asc');
-
-        if (!$sortField && !request('search')) {
+    
+        if (!$sortField && !$request->search) {
             $sortField     = 'earliest_created_at';
             $sortDirection = 'desc';
         }
@@ -30,11 +36,12 @@ class ExportsController
 
         $exports = $query->paginate(request('perPage'));
 
-        return ExportResource::collection($exports);
-    }
-
-    public function show(Export $export)
-    {
-
+        return (new ExportCollection($exports))
+            ->columnPreferenceKey('exports.columns')
+            ->additional([
+                'meta' => [
+                    'activeFilterBadges' => $activeFilterBadges,
+                ]
+            ]);
     }
 }
